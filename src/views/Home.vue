cb6174a530983318054329a46b83dff3c495a43b
crypt096 <acvjetan@gmail.com>
2021-02-09 12:10:36 +0100

    Add basic animations.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <Toast v-if="showToast" />
     <Todos @badValue="triggerToast" />
+    <transition name="fade">
+      <div v-if="showP">hello, ninjas</div>
+    </transition>
+
+    <button @click="showP = !showP">Toggle</button>
   </div>
 </template>
 
@@ -13,18 +18,39 @@ import Todos from '../components/Todos'
 export default {
   components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
+    const showP = ref(false);
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
+/* Fade in */
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+.fade-enter-active{
+  transition: all 2s ease;
+}
 
+/* Fade out */
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-leave-active{
+  transition: all 2s ease;
+}
 </style>
